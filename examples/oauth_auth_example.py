f8099ee329bd369f2fa29583219adad2df637250
mehanig <mehanig@gmail.com>
2016-05-26 03:59:01 +0300

    py test for oauth

--- examples/oauth_auth_example.py
+++ examples/oauth_auth_example.py
@@ -4,8 +4,8 @@ import requests
 
 # 1. Get your keys at https://stepic.org/oauth2/applications/ (client type = confidential,
 # authorization grant type = client credentials)
-client_id = ...
-client_secret = ...
+client_id = "..."
+client_secret = "..."
 
 # 2. Get a token
 auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
--- test_examples.py
+++ test_examples.py
@@ -1,2 +1,20 @@
-def test_dummy_fail():
-    assert 0 == 1 
+def test_oauth_auth_example():
+    import ast
+    import _ast
+    import codegen
+
+    client_id = "n4mnzQGfDEfOhFixwBvLV2mZJJLvf86pzfMMiPF5"
+    client_secret = "40ON9IPJRDAngUkVbGBTEjCBAwc2wB7lV8e71jJUPKabdKq6KBTUBKb1xGkh82KtAI1AqISrL3Zi4sTfhCBVh27YvlV6Y5klpXXV5loUWvuhMSRiN3HRZzVDO0fLBibv"
+
+    with open("examples/oauth_auth_example.py", "r") as f:
+        data = f.read()
+        p = ast.parse(data)
+    for node in p.body:
+        if type(node) == _ast.Assign:
+            if node.targets[0].id == 'client_id':
+                node.value.s = client_id
+            if node.targets[0].id == 'client_secret':
+                node.value.s = client_secret
+    ls = {}
+    exec(codegen.to_source(p), ls)
+    assert ls['course']['courses'][0]['id'] == 67
