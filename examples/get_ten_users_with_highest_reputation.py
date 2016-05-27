d89c265a960588be382ff2ffea045c58bb2442a2
ValeriyaSinevich <Valeriya.Sinevich@phystech.edu>
2016-05-27 20:11:44 +0300

    example
    get ten users with highest reputation

--- examples/get_ten_users_with_highest_reputation.py
+++ examples/get_ten_users_with_highest_reputation.py
@@ -1,14 +1,8 @@
 
 # coding: utf-8
-
-# In[13]:
-
 import json
 import requests
 from operator import itemgetter
-
-
-# In[14]:
 
 # Enter parameters below:
 # 1. Get your keys at https://stepic.org/oauth2/applications/ (client type = confidential,
@@ -22,75 +16,27 @@ resp = requests.post('https://stepic.org/oauth2/token/',
                      data={'grant_type': 'client_credentials'},
                      auth=auth
                      )
-
-
-# In[15]:
-
 token = json.loads(resp.text)['access_token']
 
+def get_users(tok):
+    page = 1
+    reputations = []
+    logins = []
+    api_url = 'https://stepic.org/api/users?page=' + str(page);
+    result = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer ' + tok}).text)
+    while(result['meta']['has_next'] == True):
+        page += 1
+        profiles = result['users']
+        for profile in profiles:
+            reputations.append(profile['reputation'])
+            name = profile['first_name'] + " " + profile['last_name']
+            logins.append(name)
+            api_url = 'https://stepic.org/api/users?page=' + str(page);
+            result = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer '+ tok}).text)
+    return reputations, logins
 
-# In[16]:
-
-page = 1
-
-
-# In[17]:
-
-api_url = 'https://stepic.org/api/users?page=' + str(page);
-
-
-# In[18]:
-
-result = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer '+ token}).text)
-
-
-# In[19]:
-
-#print(users)
-
-
-# In[25]:
-
-indices = []
-names = []
-
-
-# In[12]:
-
-while(result['meta']['has_next'] == True):
-    page += 1
-    profiles = result['users']
-    for profile in profiles:
-        indices.append(profile['reputation'])
-        name = profile['first_name'] + " " + profile['last_name']
-        names.append(name)
-        api_url = 'https://stepic.org/api/users?page=' + str(page);
-        result = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer '+ token}).text)
-    #print(users)
-
-
-# In[27]:
-
+indices, names = get_users(token)
 users = [list(c) for c in zip(indices, names)]
-
-
-# In[28]:
-
-print(users)
-
-
-# In[32]:
-
 users.sort(key=itemgetter(0), reverse=True)
-
-
-# In[33]:
-
 for i in range(10):
     print(users[i])
-
-
-# In[ ]:
-
-
-
