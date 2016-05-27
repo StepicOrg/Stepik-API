d9c689b94cf45be314ea5886571b4484463859e9
Nikolay Vyahhi <vyahhi@gmail.com>
2016-05-27 05:18:46 -0400

    Update save_course_steps.py

--- examples/save_course_steps.py
+++ examples/save_course_steps.py
@@ -33,7 +33,6 @@ def fetch_objects(api_host, obj_class, obj_ids):
   for i in range(0, len(obj_ids), 30): # fetch objects by 30 items, so we won't bump into HTTP request length limits
     obj_ids_slice = obj_ids[i : i+30]
     api_url = '{}/api/{}s?{}'.format(api_host, obj_class, '&'.join('ids[]={}'.format(obj_id) for obj_id in obj_ids_slice))
-    print(api_url)
     response = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer '+ token}).text)
     objs.extend(response['{}s'.format(obj_class)])
   return objs
