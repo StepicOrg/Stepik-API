5f11c0a2e92862c94d58c9d143e4c88596f7e2e6
greg <zimin_grigory@hotmail.com>
2016-05-28 14:22:55 +0300

    add checking for auth response
    Signed-off-by: greg <zimin_grigory@hotmail.com>

--- examples/oauth_auth_example.py
+++ examples/oauth_auth_example.py
@@ -4,8 +4,8 @@ import requests
 
 # 1. Get your keys at https://stepic.org/oauth2/applications/ (client type = confidential,
 # authorization grant type = client credentials)
-client_id = "..."
-client_secret = "..."
+client_id ="gtz2Zo4Itm1CDqSRHmLSIaA4dKCukiTwWgcs1VB6"
+client_secret =" vnbKuO0oAfW1iUFSwsHJshj2gtOEFRZtpo60bg7oDW9XM0WyGJJH2uvHXNgHjTYWsfsE38J8t2F2s3JAHgag17nkRj8EVM51IqoLQAwCdYcGcATeRJp1rDV43EsbkHX9"
 
 # 2. Get a token
 auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
@@ -13,11 +13,18 @@ resp = requests.post('https://stepic.org/oauth2/token/',
                      data={'grant_type': 'client_credentials'},
                      auth=auth
                      )
-token = json.loads(resp.text)['access_token']
+
+respDict = json.loads(resp.text);
+if (respDict.get('access_token', None) == None):
+    print("can not auth with this id and secret")
+    exit()
+
+token = respDict['access_token']
 
 # 3. Call API (https://stepic.org/api/docs/) using this token.
 # Example:
-api_url = 'https://stepic.org/api/courses/67'
-course = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer '+ token}).text)
 
-print(course)
+api_url = 'https://stepic.org/api/cource/67'
+course = json.loads(requests.get(api_url, headers={"Authorization": "Bearer"+ token}).text)
+
+#print(course)
