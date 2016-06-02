7e7a44b66c28f35728a1ee7e4730c90dd09d70b6
chisler <alexander.chisler@gmail.com>
2016-06-02 13:30:14 +0300

    renamed:    lessons_api_example (copy).py -> lessons_api_example.py

--- examples/lessons_api_example.py
+++ examples/lessons_api_example.py
@@ -10,8 +10,8 @@ It demonstrates how to get lessons data via StepicAPI and why it can be useful.'
 
 # 1. Get your keys at https://stepic.org/oauth2/applications/ (client type = confidential,
 # authorization grant type = client credentials)
-client_id = "Put yours here"
-client_secret = "Put yours here"
+client_id = "put_yours_here"
+client_secret = "put_yours_here"
 
 # 2. Get a token
 auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
@@ -30,21 +30,26 @@ lessons_data_frame = pd.DataFrame(lessons['lessons'])
 
 passed = lessons_data_frame['passed_by'].values
 time_to_complete = lessons_data_frame['time_to_complete'].values
-views = lessons_data_frame['viewed_by'].values
+viewed = lessons_data_frame['viewed_by'].values
 
-pylab.subplot (3, 1, 1)
-pylab.xlabel('time (s)')
-pylab.ylabel('Viewed this')
-pylab.bar (time_to_complete, views)
-pylab.title ("Comparison of viewed/passed")
 
-pylab.subplot (3, 1, 2)
-pylab.xlabel('time (s)')
-pylab.ylabel('Passed this')
-pylab.bar (time_to_complete, passed)
+# Drawing
+plt.close('all')
+f, (viewed_figure, passed_figure, difference_viwed_passed_figure) = plt.subplots(3, sharex=True, sharey=True)
 
-pylab.subplot (3, 1, 3)
-pylab.xlabel('time (s)')
-pylab.ylabel('Quited this')
-pylab.bar (time_to_complete, views-passed)
-pylab.savefig('quit.png')
+viewed_figure.bar(time_to_complete, viewed)
+viewed_figure.set_title('Comparison of viewed/passed')
+viewed_figure.set_xlabel('time (s)')
+viewed_figure.set_ylabel('Viewed this')
+
+passed_figure.bar(time_to_complete, passed)
+passed_figure.set_xlabel('time (s)')
+passed_figure.set_ylabel('Passed this')
+
+difference_viwed_passed_figure.bar(time_to_complete, viewed - passed)
+difference_viwed_passed_figure.set_xlabel('time (s)')
+difference_viwed_passed_figure.set_ylabel('Quited this')
+
+f.subplots_adjust(hspace=0)
+
+plt.savefig('quit.png')
