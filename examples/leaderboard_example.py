c8321a744edaa3e3a28fe94fed97ff54dfe44e2d
cfga <bibik.platon@yandex.ru>
2016-06-03 20:41:10 +0300

    Fix python3

--- examples/leaderboard_example.py
+++ examples/leaderboard_example.py
@@ -1,9 +1,18 @@
 import requests
+import grequests
 
-r = requests.get("https://stepic.org/api/leaders")
+r = requests.get("https://stepic.org/api/leaders")  # get the first page of leader list
 
-print "Current leaders:\n"
+leaders = ["https://stepic.org/api/users/" + str(leader["user"])
+           for leader in r.json()["leaders"]]  # form urls for leaders
 
-for leader in r.json()["leaders"]:
-    user = requests.get("https://stepic.org/api/users/" + str(leader["user"])).json()["users"][0]
-    print leader["score"], '\t', user['first_name'], user['last_name']
+scores = {l["user"]: l["score"] for l in r.json()["leaders"]}  # remember all scores
+
+rs = [grequests.get(l) for l in leaders]  # prepare all requests
+result = grequests.map(rs)  # execute requests concurrently
+
+print("Current leaders:\n")
+
+for r in result:
+    user = r.json()["users"][0]
+    print(scores[user["profile"]], '\t', user['first_name'], user['last_name'])
