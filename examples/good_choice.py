83fe40fa044b80b7be2bfbaaf860532fbfe7dfd1
Artyom <temic_m@mail.ru>
2016-06-05 01:01:12 +0300

    Update code structure.

--- examples/good_choice.py
+++ examples/good_choice.py
@@ -1,36 +1,40 @@
 import json
 import requests
 
-client_id = "n4mnzQGfDEfOhFixwBvLV2mZJJLvf86pzfMMiPF5"
-client_secret = "40ON9IPJRDAngUkVbGBTEjCBAwc2wB7lV8e71jJUPKabdKq6KBTUBKb1xGkh82KtAI1AqISrL" \
-                "3Zi4sTfhCBVh27YvlV6Y5klpXXV5loUWvuhMSRiN3HRZzVDO0fLBibv"
 
-auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
-resp = requests.post('https://stepic.org/oauth2/token/',
-                     data={'grant_type': 'client_credentials'},
-                     auth=auth
-                     )
-token = json.loads(resp.text)['access_token']
+def get_token():
+    client_id = "n4mnzQGfDEfOhFixwBvLV2mZJJLvf86pzfMMiPF5"
+    client_secret = "40ON9IPJRDAngUkVbGBTEjCBAwc2wB7lV8e71jJUPKabdKq6KBTUBKb1xGkh82KtAI1AqISrL" \
+                    "3Zi4sTfhCBVh27YvlV6Y5klpXXV5loUWvuhMSRiN3HRZzVDO0fLBibv"
 
-def get_data(page_num):
-    api_url = 'https://stepic.org/api/courses?page={}'.format(page_num)
-    course = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer ' + token}).text)
+    auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+    resp = requests.post('https://stepic.org/oauth2/token/',
+                         data={'grant_type': 'client_credentials'},
+                         auth=auth)
+    token = json.loads(resp.text)['access_token']
+    return token
+
+
+def get_data(pageNum):
+    api_url = 'https://stepic.org/api/courses?page={}'.format(pageNum)
+    course = json.loads(requests.get(api_url, headers={'Authorization': 'Bearer ' + get_token()}).text)
     return course
 
-def get_chosen_courses():
-    has_next_page = True
-    page_num = 0
-    list_of_choices = []
-    while has_next_page:
+
+def get_chosen_courses(amountOfUnits, courseLang, amountOfDiscuss):
+    pageNum = 0
+    hasNextPage = True
+    listOfChoices = []
+    while hasNextPage:
         try:
-            page_num += 1
-            page_content = get_data(page_num)
-            has_next_page = page_content['meta']['has_next']
-            courses = page_content['courses']
-            for course in courses:
-                if ((course['total_units']) > 5 and (course['language'] == 'ru')
-                    and (course['is_active'] == True) and (course['discussions_count'] > 30)):
-                    list_of_choices.append({
+            pageNum += 1
+            pageContent = get_data(pageNum)
+            hasNextPage = pageContent['meta']['has_next']
+            courses = pageContent['courses']
+            for course in courses:  # Select only active courses (courses with active session)
+                if ((course['total_units']) > amountOfUnits and (course['language'] == courseLang)
+                    and (course['is_active'] == True) and (course['discussions_count'] > amountOfDiscuss)):
+                    listOfChoices.append({
                         'course_name': course['slug'],
                         'amount_of_units': course['total_units'],
                         'language': course['language'],
@@ -38,12 +42,17 @@ def get_chosen_courses():
                         'discussions_count': course['discussions_count']
                     })
         except:
-            print ("Error exception: something was broken!")
+            print("Error exception: something was broken!")
 
-    print (list_of_choices)
+    print(listOfChoices)
+
 
 def main():
-    get_chosen_courses()
+    # Choose values of parameters for a course choice
+    # Example:
+    amountOfUnits = 5  # amount of units in a course
+    courseLang = 'ru'  # language of the chosen course
+    amountOfDiscuss = 30  # number of discussions in a course (as an indicator of the popularity)
+    get_chosen_courses(amountOfUnits, courseLang, amountOfDiscuss)
 
-if __name__ == '__main__':
-    main()
+main()
