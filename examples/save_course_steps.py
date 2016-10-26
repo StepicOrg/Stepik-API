93fc7afa2c4259d2fb1f9854a77229f80a2be255
Nikolay Gusev <n.a.gusev@gmail.com>
2016-10-26 23:28:35 +0300

    Write steps in correct order, add titles. Fix StepicOrg/Stepik-API/issues/49

--- examples/save_course_steps.py
+++ examples/save_course_steps.py
@@ -45,24 +45,62 @@ def fetch_objects(obj_class, obj_ids):
         objs += response['{}s'.format(obj_class)]
     return objs
 
+def dictize(objs, key):
+    objs_dict = {}
+    for obj in objs:
+        objs_dict[obj[key]] = obj
+    return objs_dict
 
 course = fetch_object('course', course_id)
+section_ids = course['sections']
 sections = fetch_objects('section', course['sections'])
+sections_dict = dictize(sections, 'id')
 
 unit_ids = [unit for section in sections for unit in section['units']]
 units = fetch_objects('unit', unit_ids)
+units_dict = dictize(units, 'id')
 
 lesson_ids = [unit['lesson'] for unit in units]
 lessons = fetch_objects('lesson', lesson_ids)
+lessons_dict = dictize(lessons, 'id')
 
 step_ids = [step for lesson in lessons for step in lesson['steps']]
 steps = fetch_objects('step', step_ids)
+steps_dict = dictize(steps, 'id')
 
+head = """<head>
+          <meta charset="UTF-8">
+          <script type="text/javascript" async
+          src="https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML">
+          </script>
+          <script type="text/x-mathjax-config">
+          MathJax.Hub.Config({
+            tex2jax: {inlineMath: [['$','$'], ['\\\\(','\\\\)']]}
+          });
+          </script>
+          </head>"""
+
+sectioncounter = 0
 with open('course{}.html'.format(course_id), 'w', encoding='utf-8') as f:
-    for step in steps:
-        text = step['block']['text']
-        url = '<a href="https://stepik.org/lesson/{}/step/{}">{}</a>'\
-            .format(step['lesson'], step['position'], step['id'])
-        f.write('<h1>{}</h1>'.format(url))
-        f.write(text)
-        f.write('<hr>')
+    f.write(head)
+    for secid in section_ids:
+        sectioncounter += 1
+        section = sections_dict[secid]
+        title = section['title']
+        f.write('<h1>{}. {}</h1>'.format(sectioncounter, title))
+        lessoncounter = 0
+        for uid in section['units']:
+            lessoncounter += 1
+            unit = units_dict[uid]
+            lid = unit['lesson']
+            lesson = lessons_dict[lid]
+            title = lesson['title']
+            f.write('<h1>{}.{} {}</h1>'.format(sectioncounter, lessoncounter, title))
+            for sid in lesson['steps']:
+                step = steps_dict[sid]
+                text = step['block']['text']
+                url = '<a href="https://stepik.org/lesson/{}/step/{}">{}</a>'\
+                    .format(step['lesson'], step['position'], step['id'])
+                f.write('<h1>{}</h1>'.format(url))
+                f.write(text)
+                f.write('<hr>')
