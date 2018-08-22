1e053eac4d7be80b9a9c497ee1fec9092658f51f
Igor Strebezhev <xamgore@users.noreply.github.com>
2018-08-22 17:10:40 +0300

    Update top_lessons_to_html.py
    There is no more `top-lessons` resource, so we have to use `lessons` ordered by `vote_delta` parameter.

--- examples/top_lessons_to_html.py
+++ examples/top_lessons_to_html.py
@@ -1,93 +1,52 @@
 import requests
 
 
-def top_lessons_json(page):
-    return requests.get("https://stepik.org:443/api/top-lessons?page={}".format(page)).json()
-
-
-def top_lessons_indices():
-    idxs = []
-    cur_page = 1
-
-    while True:
-        r = top_lessons_json(cur_page)
-
-        for top_lesson in r["top-lessons"]:
-            idxs.append(top_lesson["lesson"])
-
-        if not r["meta"]["has_next"]:
-            break
-
-        cur_page += 1
-
-    return idxs
-
-
-def lesson_info(idx):
-    r = requests.get("https://stepik.org:443/api/lessons/{}".format(idx)).json()
-    lesson = r["lessons"][0]
-
-    return lesson["title"], lesson["viewed_by"], lesson["passed_by"]
-
-
 def generate_html(top_lessons):
-    html_container = []
-
-    header = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset=utf-8 />
-        <title>Top lessons sorted by number of views</title>
-    </head>
-    """
-    html_container.append(header)
-
-    body = """
-    <body>
-    <table border = '1' align = 'center'>
-        <tr>
-            <th>Course title</th>
-            <th>Viewed by</th>
-            <th>Passed by</th>
-        </tr>
-    """
-    html_container.append(body)
-
-    lesson_template = """
-        <tr>
-            <td>{0}</td>
-            <td>{1}</td>
-            <td>{2}</td>
-        </tr>
-    """
+    rows = ""
 
     for lesson in top_lessons:
-        html_container.append(lesson_template.format(*lesson))
+        rows += """<tr>
+            <td><a href="https://stepik.org/lesson/{id}">{title}</a></td>
+            <td>{viewed_by}</td>
+            <td>{passed_by}</td>
+            <td>{vote_delta}</td>
+        </tr>""".format(**lesson)
 
-    footer = """
-    </table>
-    </body>
-    </html>
+    template = f"""
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset=utf-8 />
+            <title>Top lessons sorted by number of views</title>
+        </head>
+        <body>
+        <table border="1" align="center">
+            <tr>
+                <th>Course title</th>
+                <th>Viewed by</th>
+                <th>Passed by</th>
+                <th>Vote delta</th>
+            </tr>
+            {rows}
+        </table>
+        </body>
+        </html>
     """
-    html_container.append(footer)
 
-    return "".join(html_container)
+    return template
 
 
 def main():
-    idxs = top_lessons_indices()
+    # To sort by votes we use "order" query parameter
+    top_lessons = requests.get("https://stepik.org:443/api/lessons?order=-vote_delta").json()["lessons"]
 
-    top_lessons = []
-
-    for idx in idxs:
-        top_lessons.append(lesson_info(idx))
-
-    top_lessons = sorted(top_lessons, key=lambda x: x[1], reverse=True)
+    # Sort by number of views
+    lessons = sorted(top_lessons, key=lambda x: x["viewed_by"], reverse=True)
 
     # Generate html and write it to file
     with open("top_lessons.html", "w+") as f:
-        f.write(generate_html(top_lessons))
+        f.write(generate_html(lessons))
+
 
 if __name__ == "__main__":
     main()
